2500061c6af74f5d7cd0585c7b2f6b7dcd520129
Omkar Jadhav <omkarjadhav222004@gmail.com>
2026-08-19 20:21:43 +0530

    add data

---
+++ Tech_with_Ratan/basic/data_type.py
@@ -0,0 +1,30 @@
+name='omkar'
+print(type(name))
+
+age=22
+print(type(age))
+
+wgight=67.5
+print(type(wgight))
+
+friends=['Shailendra','vivek','rohan']
+print(type(friends))
+
+c=23+67j
+print(type(c))
+
+set_n={'om','vi','shai','ro'}
+print(type(set_n))
+
+
+info_dict={
+    'name':'omkar',
+    'a':23,
+    'm_no':7822889258,
+}
+
+print(type(info_dict))
+
+
+tup=('ram','sham',213,4435.34)
+print(type(tup))
