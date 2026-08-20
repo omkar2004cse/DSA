34ede3fb3effffd13bc9510681ccd562eea7f7ed
Omkar Jadhav <omkarjadhav222004@gmail.com>
2026-08-20 19:34:20 +0530

    Positive, Negative or Zero,Largest of Two Numbers,Largest of Three Numbers

---
+++ ChatGPT/Day1/15.py
@@ -0,0 +1,10 @@
+# Positive, Negative or Zero
+
+num=int(input("Enter a Number:-"))
+
+if num==0:
+    print("Number is Zero")
+elif num >0:
+    print("Number is Positive")
+else:
+    print("Number is Negative")
---
+++ ChatGPT/Day1/16.py
@@ -0,0 +1,8 @@
+# Largest of Two Numbers
+
+number1=int(input("Enter a 1st Number:-"))
+number2=int(input("Enter a 2nd Number:-"))
+if number1>number2:
+    print(number1,"is greater than",number2)
+else:
+    print(number2,"is greater than",number1)
