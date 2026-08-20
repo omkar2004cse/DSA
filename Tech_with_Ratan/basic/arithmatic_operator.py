7fa3fe19bc27bd59b68100f7377bd6eba1040b60
Omkar Jadhav <omkarjadhav222004@gmail.com>
2026-08-20 20:50:55 +0530

    Arithmatic Operator

---
+++ Tech_with_Ratan/basic/arithmatic_operator.py
@@ -0,0 +1,12 @@
+# Arithmatic operator
+a=int(input("Enter 1st Number:-"))
+b=int(input("Enter 2nd Number:-"))
+print("Arthimatic opearator:-")
+
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
+print(a,"**",b,"=",a**b)
+print(a,"//",b,'=',a//b)
+print(a,"%",b,"=",a%b)
